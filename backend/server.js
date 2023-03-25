798d23491e059484e35cb12891d1e5c5a6c3c0f1
Topeetee <temitopeakinwekomi28@gmail.com>
2023-03-25 16:06:36 -0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -11,13 +12,16 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+const exrcisesRouter = require('./routes/exercises');
+const userrouter = require ('./routes/users');
+app.use('/exercises', exrcisesRouter);
+app.use('/users', userrouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
